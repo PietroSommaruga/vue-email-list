4433b566bcdf8cdd3b345a5cf6d837ac307a6849
PietroSommaruga <pietro.sommaruga2001@gmail.com>
2022-01-24 17:24:27 +0100

    genoro 10 indirizzi

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+
+new Vue({
+    el: "#app",
+    data: {
+        emails:[]
+    },
+    methods: {},
+    mounted() {
+
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+                this.emails.push(result.data.response)
+        }
+    )}}
+})
+        
+
+
+
